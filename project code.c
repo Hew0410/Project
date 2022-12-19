31351674f3338534a949d90b28f8f16dbf714373
HEW YEE HANG <hewyeehang3456@gmail.com>
2022-12-20 02:42:53 +0800

    Input and output file

--- project code.c
+++ project code.c
@@ -3,5 +3,33 @@
 
 int main()
 {
+    printf("===============================\n");
+    printf("MOI CALCULATOR\n");
+    printf("-------------------------------\n");
+
+    char input_file[100];
+    char output_file[100];
+    printf("Please enter input file name:");
+    reenter:
+    scanf("%s",input_file);
+
+    //the function strcmp means "string compare, if string are equal, then the value will return to 0
+    //the specification format of strcmp is strcmp(<var name>,"<string>')==0
+    if(strcmp(input_file,"input.txt")==0)
+    {
+        printf("Please enter output file name:");
+        scanf("%s",&output_file);
+    }
+    else
+    {
+        printf("ERROR:Input file not exist...\n");
+        printf("Please re-enter input file name:");
+        goto reenter;
+    }
+
+    FILE*input;
+    input=fopen("input.txt","r");
+
+
 
 }
